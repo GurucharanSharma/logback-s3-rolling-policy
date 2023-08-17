f7145b83595038a52083f95f6046d09bfa17787a
Gurucharan Sharma <guru.thecharan@gmail.com>
2023-08-17 10:59:13 +0530

    Add files via upload

---
+++ src/main/java/com/gurucharan/logbacks3rollingpolicy/LogbackS3RollingPolicyApplication.java
@@ -0,0 +1,13 @@
+package com.gurucharan.logbacks3rollingpolicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackS3RollingPolicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackS3RollingPolicyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gurucharan/logbacks3rollingpolicy/LogbackS3RollingPolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurucharan.logbacks3rollingpolicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackS3RollingPolicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
